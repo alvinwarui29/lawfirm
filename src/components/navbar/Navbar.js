acd3104d3f3ce5e39d0d24773333046e23b87340
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-24 16:00:02 +0300

    finish add navlinks

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,10 +1,20 @@
 import './navbar.css';
 import React from 'react'
-
+import { Link } from 'react-router-dom';
+import logo from '../../assets/logo.png';
 const Navbar = () => {
   return (
     <div className="header">
-        
+        <Link to='/'>
+            {/* <img src={logo} alt="" /> */}
+            <h2>VmRanda</h2>
+        </Link>
+        <ul>
+        <Link to='/'>Home</Link>
+            <Link to='about' >About us</Link>
+            <Link to='services'>Services</Link>
+            <Link to='contact' >Contact Us</Link>
+        </ul>
     </div>
   )
 }
