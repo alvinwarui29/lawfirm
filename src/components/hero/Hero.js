be8a44b297e032a4f0629cb4a396ce058c0c902c
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-24 18:59:26 +0300

    add background image

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -1,9 +1,19 @@
 import './hero.css';
-import React from 'react'
+import React from 'react';
+import hero from '../../assets/hero.jpg'
+import logo from '../../assets/logo.png'
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div className="hero">
+    <div className="mask">
+      <img src={hero} alt="" className='into-img' />
+    </div>
+      <div className="left-h">
+          <img src={logo} alt="" />
+      </div>
+      <div className="right-h"></div>
+    </div>
   )
 }
 
