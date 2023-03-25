7b60b7cfe2429c20c979078d81494c652e4d5fd4
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-25 23:34:02 +0300

    seection's starting point

--- src/routes/home.js
+++ src/routes/home.js
@@ -3,12 +3,14 @@ import Hero from '../components/hero/Hero'
 import Footer from '../components/footer/Footer'
 import Navbar from '../components/navbar/Navbar'
 import WhyUs from '../components/whyus/WhyUs'
+import About from '../components/about/About'
 const home = () => {
   return (
     <div>
     <Navbar/>
     <Hero/>
     <WhyUs/>
+    <About/>
     <Footer/>
     
     </div>
