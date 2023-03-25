8b4d2b9d7331d5d7f2f244598b71fb98e909da39
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-25 23:21:20 +0300

    finish styling

--- src/components/whyus/WhyUs.js
+++ src/components/whyus/WhyUs.js
@@ -15,24 +15,24 @@ const WhyUs = () => {
                 <p>Here at VmRanda & Co., we value honesty and openness when it comes to pricing.You can rely on there being no unforeseen costs or hidden fees and only being charged for the services you actually require.</p>
             </div>
             <div className="card">
-                <h3>Compassionate approach:</h3>
-                <span className="bar"></span>
-                <p>We are aware of how demanding and emotionally taxing legal matters may be.Because of this, we approach every situation we handle with compassion. We think that delivering excellent legal representation and demonstrating empathy and understanding for our clients go hand in hand </p>
-            </div>
-            <div className="card">
                 <h3>Many years of experience</h3>
                 <span className="bar"></span>
                 <p>We have years of expertise working with customers in many different practice areas to provide legal services. Our legal team has experience with a wide range of matters, from straightforward ones to more complicated ones.</p>
             </div>
             <div className="card">
-                <h3>Personalized service</h3>
-                <span className="bar"></span>
-                <p> We are aware of the individuality of every client and think that each should receive individualized legal services. We provide each of our customers with individualized service as a result. Our lawyers take the time to get to know each client, to comprehend their problems and aspirations, and to create a legal plan that is specific to their particular case.</p>
-            </div>
-            <div className="card">
                 <h3>Responsive communication</h3>
                 <span className="bar"></span>
                 <p> To develop enduring relationships with our clients, we think effective communication is essential. We place a high value on responsive communication throughout the legal procedure because of this</p>
+            </div>
+            <div className="card">
+                <h3>Compassionate approach:</h3>
+                <span className="bar"></span>
+                <p>We are aware of how demanding and emotionally taxing legal matters may be.Because of this, we approach every situation we handle with compassion. We think that delivering excellent legal representation and demonstrating empathy and understanding for our clients go hand in hand </p>
+            </div>
+            <div className="card">
+                <h3>Personalized service</h3>
+                <span className="bar"></span>
+                <p> We are aware of the individuality of every client and think that each should receive individualized legal services. We provide each of our customers with individualized service as a result. Our lawyers take the time to get to know each client, to comprehend their problems and aspirations, and to create a legal plan that is specific to their particular case.</p>
             </div>
             <div className="card">.
                 <h3>Specialized expertise</h3>
