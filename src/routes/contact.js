76bed29a25428ac71985ce7e1895e70052ca04c3
alvinwarui29 <alvinwarui@kabarak.ac.ke>
2023-03-28 03:54:40 +0300

    finish passing image dynamically

--- src/routes/contact.js
+++ src/routes/contact.js
@@ -3,11 +3,12 @@ import Hero from '../components/hero/Hero'
 import Navbar from '../components/navbar/Navbar'
 import Footer from '../components/footer/Footer'
 import Contact from '../components/contact/Contact'
+import contacte from '../assets/contacte.jpg'
 const contact = () => {
   return (
     <div>
       <Navbar/>
-      <Hero heading="Contact us today" text ="Kindly get in touch with us by filling the form below"/>
+      <Hero img={contacte} heading="Contact us today" text ="Kindly get in touch with us by filling the form below"/>
       <Contact/>
       <Footer/>
     </div>
